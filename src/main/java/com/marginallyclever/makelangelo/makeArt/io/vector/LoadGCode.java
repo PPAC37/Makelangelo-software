b49144567cfebdafadee146de884eb95daf98e93
PPAC37 <pierre.pacaud.37@gmail.com>
2022-02-05 04:19:49 +0100

    list image io fileWriter to simple lower string

--- src/main/java/com/marginallyclever/makelangelo/makeArt/io/vector/LoadGCode.java
+++ src/main/java/com/marginallyclever/makelangelo/makeArt/io/vector/LoadGCode.java
@@ -36,6 +36,8 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.logging.Level;
 import java.util.prefs.Preferences;
 import javax.imageio.ImageIO;
@@ -367,138 +369,144 @@ public class LoadGCode implements TurtleLoader {
 	private static Logger logger = LoggerFactory.getLogger(LoadGCode.class);
 
 	public static void main(String[] args) {
-
-	    boolean doFullJFrame = true;
-	    // 
-	    Log.start();
-	    // lazy init to be able to purge old files
-	    //logger = LoggerFactory.getLogger(Makelangelo.class);
-
-	    PreferencesHelper.start();
-	    CommandLineOptions.setFromMain(args);
-	    Translator.start();
-
-	    if (Translator.isThisTheFirstTimeLoadingLanguageFiles()) {
-		LanguagePreferences.chooseLanguage();
-	    }
-
-	    //setSystemLookAndFeel();
-	    Makelangelo.setSystemLookAndFeel(); // modified to be public
-
-	    javax.swing.SwingUtilities.invokeLater(() -> {
 		
-				Rectangle rFrame = null;
-		try {
-		    Makelangelo makelangeloProgram = new Makelangelo();
-		    //some hack ...
-		    makelangeloProgram.doNotAskIfOkOnExit = true;
-		    makelangeloProgram.doNotShowLoadDialogu = true;
-		    //to use it witout jframe ...
+		//listImageTypeSupported();
+		listImageTypeSupportedAsSimpleLowerCaseString();
+		boolean doFullJFrame = true;
+		// 
+		Log.start();
+		// lazy init to be able to purge old files
+		//logger = LoggerFactory.getLogger(Makelangelo.class);
 
-		    Component cToScreenShoot = null;
-		    if (doFullJFrame) {
-			makelangeloProgram.run();//in CI throws HeadlessException as it create a new JFrame ... and display it
+		PreferencesHelper.start();
+		CommandLineOptions.setFromMain(args);
+		Translator.start();
 
-			JFrame mainFrame = makelangeloProgram.getMainFrame();
-			//mainFrame.getRootPane();
-			// n.b. if using a JFrame / Frame we do not get the OS décoration ( window bar title ... )
-			//
-			cToScreenShoot = (Component) mainFrame;
-			cToScreenShoot = (Component) mainFrame.getRootPane(); // have to set a size !
-			//
-			
-			rFrame = new Rectangle(mainFrame.getLocation(),mainFrame.getSize());
-
-		    } else {
-			// if i do not whant the JFrame to be created ...
-			//https://stackoverflow.com/questions/17026803/headless-painting
-			cToScreenShoot = makelangeloProgram.createContentPane();
-			// /!\ the component have to have a setSize seted or else later in paintAll a 0,0 sized component will create an exception.
-			//cToScreenShoot.setSize(cToScreenShoot.getPreferredSize());
-			cToScreenShoot.addNotify();// Needed in an headless env to doLayout recursively of all sub component			
-			cToScreenShoot.doLayout();
-			// Needed : 
-			makelangeloProgram.camera.zoomToFit(Paper.DEFAULT_WIDTH, Paper.DEFAULT_HEIGHT);
-			
-		    }
-		    Dimension dim = new Dimension(480, 640);
-		    rFrame.setSize(dim);
-		    cToScreenShoot.setSize(dim);
-
-		    // a way to do a kind of print screen ...
-		    // load a .gcode (test set todo)
-		    makelangeloProgram.openLoadFile("/home/q6/g2_g3_test.gcode");
-		    // n.b. if in a headlees env will throw an Exception if some dialog (message/error/...) a thrown ...
-
-		    //https://stackoverflow.com/questions/19621105/save-image-from-jpanel-after-draw/19621211
-		    //https://stackoverflow.com/questions/31744516/save-a-drawn-picture-on-a-jpanel-in-a-file-java
-		    BufferedImage bufferImage = new BufferedImage(cToScreenShoot.getWidth(), cToScreenShoot.getHeight(),
-			    BufferedImage.TYPE_INT_ARGB);
-		    Graphics2D bufferGraphics = bufferImage.createGraphics();
-		    // Clear the buffer:
-		    bufferGraphics.clearRect(0, 0, cToScreenShoot.getWidth(), cToScreenShoot.getHeight());
-		    //define some rendering option (optional ?)
-		    bufferGraphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		    bufferGraphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-		    // make the JComponent paint on the bufferedGraphics to get a copy of the render.
-		    cToScreenShoot.paintAll(bufferGraphics);
-		    // If you whant to add some to the normaly paint result ( like a date)
-		    BasicStroke stroke = new BasicStroke(4);
-		    bufferGraphics.setStroke(stroke);
-		    bufferGraphics.setColor(Color.RED);
-		    bufferGraphics.drawString("Coucou", cToScreenShoot.getWidth() / 2, cToScreenShoot.getHeight() / 2);
-		    // save the "image" painted in the bufferImage to a file
-		    ImageIO.write(bufferImage, "png", new File("cframeImage.png"));
-    //		    // and save a resize image to
-    //		    int dimW = cToScreenShoot.getWidth() / 2;
-    //		    int dimH = cToScreenShoot.getHeight() / 2;
-    //		    Image scaledInstance = bufferImage.getScaledInstance(dimW, dimH, Image.SCALE_DEFAULT);
-    //		    ImageIO.write(convertToBufferedImage(scaledInstance), "png", new File("cframeImage_vignette.png"));
-		    //
-		final Rectangle rFramef = rFrame;
-		    javax.swing.SwingUtilities.invokeLater(() -> {
-
-			try {
-			    if (doFullJFrame) {
-				//https://stackoverflow.com/questions/58305/is-there-a-way-to-take-a-screenshot-using-java-and-save-it-to-some-sort-of-image
-				//	18
-				//If you'd like to capture all monitors, you can use the following code:
-				GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-				GraphicsDevice[] screens = ge.getScreenDevices();
-
-				Rectangle allScreenBounds = new Rectangle();
-				for (GraphicsDevice screen : screens) {
-				    Rectangle screenBounds = screen.getDefaultConfiguration().getBounds();
-
-				    allScreenBounds.width += screenBounds.width;
-				    allScreenBounds.height = Math.max(allScreenBounds.height, screenBounds.height);
-				}
-
-				//Rectangle 					rFrame = null;
-				if ( rFramef != null){
-				    allScreenBounds = rFramef;
-				}
-				Robot robot = new Robot();
-				//robot.waitForIdle();//?
-				BufferedImage screenShot = robot.createScreenCapture(allScreenBounds);
-				ImageIO.write(screenShot, "png", new File("screenshot_robot.png"));
-			    }
-			} catch (AWTException ex) {
-			    java.util.logging.Logger.getLogger(LoadGCode.class.getName()).log(Level.SEVERE, null, ex);
-			} catch (IOException ex) {
-			    java.util.logging.Logger.getLogger(LoadGCode.class.getName()).log(Level.SEVERE, null, ex);
-			}
-
-		    });
-		    //System.exit(0);
-		} catch (IOException ex) {
-		    java.util.logging.Logger.getLogger(LoadGCode.class.getName()).log(Level.SEVERE, null, ex);
+		if (Translator.isThisTheFirstTimeLoadingLanguageFiles()) {
+			LanguagePreferences.chooseLanguage();
 		}
 
-		//System.exit(0);
-	    });
+		//setSystemLookAndFeel();
+		Makelangelo.setSystemLookAndFeel(); // modified to be public
 
-	    //
+		javax.swing.SwingUtilities.invokeLater(() -> {
+
+			Rectangle rFrame = null;
+			try {
+				Makelangelo makelangeloProgram = new Makelangelo();
+				//some hack ...
+				makelangeloProgram.doNotAskIfOkOnExit = true;
+				makelangeloProgram.doNotShowLoadDialogu = true;
+				//to use it witout jframe ...
+
+				Component cToScreenShoot = null;
+				if (doFullJFrame) {
+					makelangeloProgram.run();//in CI throws HeadlessException as it create a new JFrame ... and display it
+
+					JFrame mainFrame = makelangeloProgram.getMainFrame();
+					//mainFrame.getRootPane();
+					// n.b. if using a JFrame / Frame we do not get the OS décoration ( window bar title ... )
+					//
+					cToScreenShoot = (Component) mainFrame;
+					cToScreenShoot = (Component) mainFrame.getRootPane(); // have to set a size !
+					//
+
+					rFrame = new Rectangle(mainFrame.getLocation(), mainFrame.getSize());
+
+				} else {
+					// if i do not whant the JFrame to be created ...
+					//https://stackoverflow.com/questions/17026803/headless-painting
+					cToScreenShoot = makelangeloProgram.createContentPane();
+					// /!\ the component have to have a setSize seted or else later in paintAll a 0,0 sized component will create an exception.
+					//cToScreenShoot.setSize(cToScreenShoot.getPreferredSize());
+					cToScreenShoot.addNotify();// Needed in an headless env to doLayout recursively of all sub component			
+					cToScreenShoot.doLayout();
+					// Needed : 
+					makelangeloProgram.camera.zoomToFit(Paper.DEFAULT_WIDTH, Paper.DEFAULT_HEIGHT);
+
+				}
+				Dimension dim = new Dimension(480, 640);
+				rFrame.setSize(dim);
+				cToScreenShoot.setSize(dim);
+
+				// a way to do a kind of print screen ...
+				// load a .gcode (test set todo)
+				try{
+					makelangeloProgram.openLoadFile("/home/q6/g2_g3_test.gcode");
+				}catch (Exception e){
+					e.printStackTrace();
+				}
+				// n.b. if in a headlees env will throw an Exception if some dialog (message/error/...) a thrown ...
+
+				//https://stackoverflow.com/questions/19621105/save-image-from-jpanel-after-draw/19621211
+				//https://stackoverflow.com/questions/31744516/save-a-drawn-picture-on-a-jpanel-in-a-file-java
+				BufferedImage bufferImage = new BufferedImage(cToScreenShoot.getWidth(), cToScreenShoot.getHeight(),
+						BufferedImage.TYPE_INT_ARGB);
+				Graphics2D bufferGraphics = bufferImage.createGraphics();
+				// Clear the buffer:
+				bufferGraphics.clearRect(0, 0, cToScreenShoot.getWidth(), cToScreenShoot.getHeight());
+				//define some rendering option (optional ?)
+				bufferGraphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+				bufferGraphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+				// make the JComponent paint on the bufferedGraphics to get a copy of the render.
+				cToScreenShoot.paintAll(bufferGraphics);
+				// If you whant to add some to the normaly paint result ( like a date)
+				BasicStroke stroke = new BasicStroke(4);
+				bufferGraphics.setStroke(stroke);
+				bufferGraphics.setColor(Color.RED);
+				bufferGraphics.drawString("Coucou", cToScreenShoot.getWidth() / 2, cToScreenShoot.getHeight() / 2);
+				// save the "image" painted in the bufferImage to a file
+				ImageIO.write(bufferImage, "png", new File("cframeImage.png"));
+				//		    // and save a resize image to
+				//		    int dimW = cToScreenShoot.getWidth() / 2;
+				//		    int dimH = cToScreenShoot.getHeight() / 2;
+				//		    Image scaledInstance = bufferImage.getScaledInstance(dimW, dimH, Image.SCALE_DEFAULT);
+				//		    ImageIO.write(convertToBufferedImage(scaledInstance), "png", new File("cframeImage_vignette.png"));
+				//
+				final Rectangle rFramef = rFrame;
+				javax.swing.SwingUtilities.invokeLater(() -> {
+
+					try {
+						if (doFullJFrame) {
+							//https://stackoverflow.com/questions/58305/is-there-a-way-to-take-a-screenshot-using-java-and-save-it-to-some-sort-of-image
+							//	18
+							//If you'd like to capture all monitors, you can use the following code:
+							GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+							GraphicsDevice[] screens = ge.getScreenDevices();
+
+							Rectangle allScreenBounds = new Rectangle();
+							for (GraphicsDevice screen : screens) {
+								Rectangle screenBounds = screen.getDefaultConfiguration().getBounds();
+
+								allScreenBounds.width += screenBounds.width;
+								allScreenBounds.height = Math.max(allScreenBounds.height, screenBounds.height);
+							}
+
+							//Rectangle 					rFrame = null;
+							if (rFramef != null) {
+								allScreenBounds = rFramef;
+							}
+							Robot robot = new Robot();
+							//robot.waitForIdle();//?
+							BufferedImage screenShot = robot.createScreenCapture(allScreenBounds);
+							ImageIO.write(screenShot, "png", new File("screenshot_robot.png"));
+						}
+					} catch (AWTException ex) {
+						java.util.logging.Logger.getLogger(LoadGCode.class.getName()).log(Level.SEVERE, null, ex);
+					} catch (IOException ex) {
+						java.util.logging.Logger.getLogger(LoadGCode.class.getName()).log(Level.SEVERE, null, ex);
+					}
+
+				});
+				//System.exit(0);
+			} catch (IOException ex) {
+				java.util.logging.Logger.getLogger(LoadGCode.class.getName()).log(Level.SEVERE, null, ex);
+			}
+
+			//System.exit(0);
+		});
+
+		//
 	}
 
 	/**
@@ -510,20 +518,20 @@ public class LoadGCode implements TurtleLoader {
 	 */
 	public static BufferedImage convertToBufferedImage(Image img) {
 
-	    if (img instanceof BufferedImage) {
-		return (BufferedImage) img;
-	    }
+		if (img instanceof BufferedImage) {
+			return (BufferedImage) img;
+		}
 
-	    // Create a buffered image with transparency
-	    BufferedImage bi = new BufferedImage(
-		    img.getWidth(null), img.getHeight(null),
-		    BufferedImage.TYPE_INT_ARGB);
+		// Create a buffered image with transparency
+		BufferedImage bi = new BufferedImage(
+				img.getWidth(null), img.getHeight(null),
+				BufferedImage.TYPE_INT_ARGB);
 
-	    Graphics2D graphics2D = bi.createGraphics();
-	    graphics2D.drawImage(img, 0, 0, null);
-	    graphics2D.dispose();
+		Graphics2D graphics2D = bi.createGraphics();
+		graphics2D.drawImage(img, 0, 0, null);
+		graphics2D.dispose();
 
-	    return bi;
+		return bi;
 	}
 
 	/**
@@ -531,8 +539,21 @@ public class LoadGCode implements TurtleLoader {
 	 * https://mkyong.com/java/how-to-write-an-image-to-file-imageio/
 	 */
 	private static void listImageTypeSupported() {
-	    String writerNames[] = ImageIO.getWriterFormatNames();
-	    Arrays.stream(writerNames).forEach(System.out::println);
+		String writerNames[] = ImageIO.getWriterFormatNames();
+		Arrays.stream(writerNames).sorted().forEach(System.out::println);
+	}
+
+	private static String listImageTypeSupportedAsSimpleLowerCaseString() {
+		String writerNames[] = ImageIO.getWriterFormatNames();
+		
+		SortedSet<String> sset = new TreeSet<>();		
+		Arrays.stream(writerNames).forEach(wn -> sset.add(wn.toLowerCase()));
+		
+		StringBuilder sb = new StringBuilder();
+		sset.forEach(s -> {sb.append(s);sb.append("/");});
+		if ( sb.length()>0 ) {sb.setLength(sb.length()-1);}
+		
+		return sb.toString();
 	}
 
 
